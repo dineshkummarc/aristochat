bcd632242b09f4e12e5b2a8daf4f38dde75c9f13
Pradeep Elankumaran <pradeep@intridea.com>
2009-07-11 20:02:17 -0700

    lots of formatting changes, cause i'm painful that way

--- chat.js
+++ chat.js
@@ -8,71 +8,60 @@ AristoChat = {
 var connection = AristoChat.connection; //
 
 function log(msg) {
-    if(DEBUG) {
-        $('#log').append('<div></div><br /><br />').append(document.createTextNode(msg));
-    }
-}
-
-function rawInput(data) {
-    log('RECV: ' + data);
-}
-
-function rawOutput(data) {
-    log('SENT: ' + data);
-}
-
+  if(DEBUG) $('#log').append('<div></div><br /><br />').append(document.createTextNode(msg)); }
+function rawInput(data) { log('RECV: ' + data); }
+function rawOutput(data) { log('SENT: ' + data); }
 
 function onMessage(msg) {
-    var from = msg.getAttribute('from');
-    var type = msg.getAttribute('type');
-    var body = msg.getElementsByTagName("body")[0].childNodes[0];
-    var user = Strophe.getResourceFromJid(from);
-    
-    if (type == "groupchat" && body) {
-        body = body.nodeValue.replace(/(<([^>]+)>)/ig,"");
-        if(user == AristoChat.nick) {
-            $('#chat').append("<tr class='message'><td class='me'>"+ user + "</td><td class='body'>" + body +"</td></tr>");            
-        }
-        else {
-            $('#chat').append("<tr class='message'><td class='nick'>"+ user + "</td><td class='body'>" + body +"</td></tr>");
-        }
-        $('html, body').animate({scrollTop: $('.message:last').offset().top }, 5);
-    }
-    // we must return true to keep the handler alive.  
-    // returning false would remove it after it finishes.
-    return true;
+	var from = msg.getAttribute('from');
+	var type = msg.getAttribute('type');
+	var body = msg.getElementsByTagName("body")[0].childNodes[0];
+	var user = Strophe.getResourceFromJid(from);
+
+	if (type == "groupchat" && body) {
+		body = body.nodeValue.replace(/(<([^>]+)>)/ig,"");
+		if(user == AristoChat.nick) {
+			$('#chat').append("<tr class='message'><td class='me'>"+ user + "</td><td class='body'>" + body +"</td></tr>");            
+		} else {
+			$('#chat').append("<tr class='message'><td class='nick'>"+ user + "</td><td class='body'>" + body +"</td></tr>");
+		}
+		$('html, body').animate({scrollTop: $('.message:last').offset().top }, 5);
+	}
+	// we must return true to keep the handler alive.  
+	// returning false would remove it after it finishes.
+	return true;
 }
 
 function user_connected(user) {
-    $('#' + user).hide("slow").remove();
-    $('#users').append("<li id='" + user + "'>"+ user +"</li>")
-    $('#chat').append("<tr class='presence'><td class='nick'>"+ user + "</td><td class='body'>has entered the room</td></tr>");
-    $('html, body').animate({scrollTop: $('.message:last').offset().top }, 5);
+  $('#' + user).hide("slow").remove();
+  $('#users').append("<li id='" + user + "'>"+ user +"</li>")
+  $('#chat').append("<tr class='presence'><td class='nick'>"+ user + 
+		"</td><td class='body'>has entered the room</td></tr>");
+  $('html, body').animate({scrollTop: $('.message:last').offset().top }, 5);
 }
 
 function user_disconnected(user) {
-    $('#' + user).hide("slow").remove();
-    $('#chat').append("<tr class='presence'><td class='nick'>"+ user + "</td><td class='body'>has left the room</td></tr>");
-    $('html, body').animate({scrollTop: $('.message:last').offset().top }, 5);    
+  $('#' + user).hide("slow").remove();
+  $('#chat').append("<tr class='presence'><td class='nick'>"+ user + 
+		"</td><td class='body'>has left the room</td></tr>");
+  $('html, body').animate({scrollTop: $('.message:last').offset().top }, 5);    
 }
 
 function onPresence(msg) {
-    var user = Strophe.getResourceFromJid(msg.getAttribute('from'));
-    var type = msg.getAttribute('type');
+  var user = Strophe.getResourceFromJid(msg.getAttribute('from'));
+  var type = msg.getAttribute('type');
     
-    if (type == "unavailable") {
-        user_disconnected(user);
-    }
-    else if (type == "error") {
-        onDisconnect();
-        alert(msg.getElementsByTagName("text")[0].childNodes[0].nodeValue);
-    }
-    else {
-        user_connected(user);
-    }
-    // we must return true to keep the handler alive.  
-    // returning false would remove it after it finishes.
-    return true;
+  if (type == "unavailable") {
+    user_disconnected(user);
+  } else if (type == "error") {
+		onDisconnect();
+		alert(msg.getElementsByTagName("text")[0].childNodes[0].nodeValue);
+  } else {
+    user_connected(user);
+  } 
+  // we must return true to keep the handler alive.  
+  // returning false would remove it after it finishes.
+  return true;
 }
 
 function onConnect(status) {
@@ -110,7 +99,8 @@ function onConnect(status) {
         connected();
         log('Strophe is connected.');
         // Entrering the room
-        connection.send($pres({to: MUC_ROOM + "@" + MUC_COMPONENT + "/" + AristoChat.nick}).c("x", {xmlns: "http://jabber.org/protocol/muc"}).tree());
+        connection.send($pres({to: MUC_ROOM + "@" + MUC_COMPONENT + "/" + AristoChat.nick}).
+					c("x", {xmlns: "http://jabber.org/protocol/muc"}).tree());
         $('#loader').hide();
     }
 }
@@ -126,9 +116,9 @@ function connected() {
 }
 
 function onDisconnect() {
-    if(AristoChat.nick != "") {
-        user_disconnected(AristoChat.nick);
-    }
+    if(AristoChat.nick != "")
+      user_disconnected(AristoChat.nick);
+   
     $('#post').attr("disabled", "disabled"); // To disable
     $('#connect').get(0).value = 'connect';
     $('#login').show();
@@ -153,10 +143,10 @@ $(document).ready(function () {
             $('#connect').attr("disabled", "disabled"); // To disable 
             $('#loader').show();
             if($('#nick').get(0).value == "") {
-                AristoChat.nick = Strophe.getNodeFromJid($('#jid').get(0).value);
+              AristoChat.nick = Strophe.getNodeFromJid($('#jid').get(0).value);
             }
             else {
-                AristoChat.nick = $('#nick').get(0).value;
+            	AristoChat.nick = $('#nick').get(0).value;
             }
             AristoChat.jid = $('#jid').get(0).value;
             AristoChat.password = $('#pass').get(0).value;
@@ -172,9 +162,15 @@ $(document).ready(function () {
 
 function sendMessageWithCallbacks() {
 	var msg = $('#'+AristoChat.inputField).get(0).value.replace(/(<([^>]+)>)/ig,"");
+
 	msg = AristoChat.onEnterNewMessageBefore(msg);
-	connection.send($msg({to: MUC_ROOM + "@" + MUC_COMPONENT, type: "groupchat", id: connection.getUniqueId}).c("body").t(msg).up().c("nick", {xmlns: "http://jabber.org/protocol/nick"}).t(AristoChat.nick).tree());
+	connection.send($msg({to: MUC_ROOM + "@" + MUC_COMPONENT, 
+		type: "groupchat", id: connection.getUniqueId
+	}).c("body").t(msg).up().
+		 c("nick", {xmlns: "http://jabber.org/protocol/nick"}).
+		 	 t(AristoChat.nick).tree());
   $('#'+AristoChat.inputField).get(0).value = "";
+
 	AristoChat.onEnterNewMessageAfter(msg);
 }
 
